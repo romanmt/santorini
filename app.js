da88b4dbdf7fd7688c0112630555c02a18f04157
romanmt <romanmt@gmail.com>
2011-03-14 08:03:29 -0400

    added new user function

--- app.js
+++ app.js
@@ -10,37 +10,53 @@ var mongoose = require('mongoose')
 // and then serve with connect's staticProvider
 
 var app = express.createServer(
-	express.compiler({ src: pub, enable: ['sass'] })
+    express.compiler({ src: pub, enable: ['sass'] })
   , express.static(pub)
+  , express.logger()
+  , express.bodyParser()
 );
 
-// setup jade as default template engine
-app.set('view engine', 'jade');
+app.configure(function(){
+    app.set('view engine', 'jade')
+  , app.set('reload views', 1000)
+});
 
 app.get('/', function(req, res){
     user.find({}, function(err, docs){
-		res.render('users', { users: docs });	
-	})
+    res.render('users', { users: docs }); 
+  })
 });
 
 app.get('/users', function(req, res){
-	user.find({}, function(err, docs){
-		console.log(docs);
-		res.partial('users/user', docs);
-	})
+  user.find({}, function(err, docs){
+    console.log(docs);
+    res.partial('users/user', docs);
+  })
 });
 
 app.get('/users/list', function(req, res){
-	user.find({}, function(err, docs){
-		res.partial('users/list', { list: docs })
-	})	
-});	
+  user.find({}, function(err, docs){
+    res.partial('users/list', { list: docs })
+  })  
+}); 
 
 app.get('/user/:id', function(req, res){
-	user.findById(req.params.id, function(err, user){
-		console.log(user);
-    	res.partial('users/user', [user]);	
-	})
+  user.findById(req.params.id, function(err, user){
+    console.log(user);
+      res.partial('users/user', [user]);  
+  })
+});
+
+app.get('/users/new', function(req, res){
+  res.partial('users/new', {});
+});
+
+app.post('/users/new', function(req, res){
+  console.log(req.body.user);
+  u = new user(req.body.user);
+  u.save(function(err){
+    res.redirect('/');
+  });
 });
 
 app.listen(3000);
