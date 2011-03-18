f8b839b169bd7e68413afed1b20596b7e6b912c5
romanmt <romanmt@gmail.com>
2011-03-17 20:42:14 -0400

    working on a delete tag helper

--- app/helpers/form_helpers.js
+++ app/helpers/form_helpers.js
@@ -10,13 +10,31 @@ app.helpers( {
             "input(type: 'text', name: obj +'[' + prop + ']').inputbox";
   
     return jade.render(html, {locals: {obj: obj, prop: prop} });
-  },
-  
-  integer_tag_for: function(obj, prop) {
+  }
+, integer_tag_for: function(obj, prop) {
     
     html = "label(for=obj + '[' + prop + ']') #{prop.decamelize()}:\n" + 
             "input(type: 'integer', name: obj +'[' + prop + ']').inputbox.smaller";
   
     return jade.render(html, {locals: {obj: obj, prop: prop} });
+  }
+, delete_tag: function(obj, id) {
+    url = "/" + obj + "/" + id; 
+    html = "<a onclick='if (confirm('Are you sure?')) { " +
+           "  var f = document.createElement('form'); " +
+           "  f.style.display = 'none'; " +
+           "  this.parentNode.appendChild(f); " +
+           "  f.method = 'POST'; " + 
+           "  f.action = this.href;" +
+           "  var m = document.createElement('input'); " + 
+           "  m.setAttribute('type', 'hidden'); " +
+           "  m.setAttribute('name', '_method'); " + 
+           "  m.setAttribute('value', 'delete'); " +
+           "  f.appendChild(m);" + 
+           "  f.submit(); " +
+           "};" +
+           "return false;' href=url>delete</a>";
+
+    return jade.render(html, {locals: {url: url}});
   }
 });
